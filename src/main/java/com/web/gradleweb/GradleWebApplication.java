377d27d946ca633cdc373944aa8ae80818cb1085
Subhash Reddy Peddyreddy <p.subhashreddy@gmail.com>
2017-08-03 16:02:47 +0200

    pushing gradle project to github

---
+++ src/main/java/com/web/gradleweb/GradleWebApplication.java
@@ -0,0 +1,12 @@
+package com.web.gradleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleWebApplication.class, args);
+	}
+}
